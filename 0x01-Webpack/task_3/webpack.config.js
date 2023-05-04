99a61261bb679cff103719a3a5866b38c614813a
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 18:53:12 +0200

    Update config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,44 +1,49 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
     entry: {
-      all: ['./modules/header/header.js', './modules/body/body.js', './modules/footer/footer.js']
+        header: './modules/header/header.js',
+        body: './modules/body/body.js',
+        footer: './modules/footer/footer.js',
     },
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, './public')
+        path: path.resolve(__dirname, "public")
     },
+    devtool: 'inline-source-map',
     devServer: {
         contentBase: './public',
         port: 8564,
         open: true
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Holberton Dashboard',
-        }),
-        new CleanWebpackPlugin()
-    ],
-    devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
-                test: /\.(png|svg|jpe?g|gif)$/i,
+                test: /\.(gif|png|jpe?g|svg)$/i,
                 type: 'asset/resource',
-                loader: 'image-webpack-loader',
-            },
-        ],
+                loader: 'image-webpack-loader'
+            }
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'task_3'
+        }),
+        new CleanWebpackPlugin()
+    ],
     optimization: {
         splitChunks: {
-            chunks: 'all',
+          chunks: 'all',
         },
     },
-};
+}
