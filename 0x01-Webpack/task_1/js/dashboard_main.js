01dd9ba671598e428a598de8a6b2a333060f54dd
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 10:52:13 +0200

    Update lodash importation

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
