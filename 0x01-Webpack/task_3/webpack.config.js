5df7f653fe019347883f647447c0b617e1eabc0d
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 18:49:46 +0200

    Fix config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -5,9 +5,7 @@ const path = require('path');
 module.exports = {
     mode: "development",
     entry: {
-        header: './modules/header/header.js',
-        body: './modules/body/body.js',
-        footer: './modules/footer/footer.js',
+        all: ['./modules/header/header.js', './modules/body/body.js', './modules/footer/footer.js']
     },
     output: {
         filename: "[name].bundle.js",
@@ -32,6 +30,7 @@ module.exports = {
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 type: 'asset/resource',
+                loader: 'image-webpack-loader'
             }
         ]
     },
