da9b5e5ed17f56c0d0af31eccf95bddbeeebe0b1
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 10:48:00 +0200

    Try to update webpack.config.js

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -2,16 +2,9 @@ const path = require('path');
 
 module.exports = {
     entry: "./js/dashboard_main.js",
+    mode: "production",
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-            },
-        ]
     },
 }
