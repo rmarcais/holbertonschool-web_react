850e2ac897adb9b149f333d449fa0b2e3de1cc7b
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 16:18:04 +0200

    Go back to the previous version

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,35 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-      main: path.resolve(__dirname, "./js/dashboard_main.js")
-    },
+    mode: "production",
+    entry: "./js/dashboard_main.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './public')
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public")
     },
     module: {
-        rules : [
+        rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
-                /* test: /\.(png|svg|jpe?g|gif)$/i,
-                type: 'asset/resource',
-                loader: 'image-webpack-loader', */
-                test: /\.(png|svg|jpe?g|gif)$/i,
-                use: [
-                    'file-loader',
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassingOnDebug: true,
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-};
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                type: 'asset/ressource'
+            }
+        ]
+    }
+}
