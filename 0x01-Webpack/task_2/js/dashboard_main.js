461b23eb24b5ebc60fd104327ca9bd91046c07ee
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-04 16:34:54 +0200

    Go back to the previous version

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,19 @@
-import '../css/main.css';
+import '../css/main.css'
 import $ from 'jquery';
-import _ from "lodash";
+import _ from 'lodash';
 
 $('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button type="button">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-function updateCounter() {
-    let count = 0;
-    $('button').on('click', _.debounce(() => {
-        count++;
-        $('#count').text(`${count} clicks on the button`);
-    }, 500));
-}
+let count = 0;
 
-updateCounter();
+$('button').on("click", _.debounce(updateCounter, 500))
+
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,35 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-      main: path.resolve(__dirname, "./js/dashboard_main.js")
-    },
+    mode: "production",
+    entry: "./js/dashboard_main.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './public')
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public")
     },
     module: {
-        rules : [
+        rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
-                /* test: /\.(png|svg|jpe?g|gif)$/i,
-                type: 'asset/resource',
-                loader: 'image-webpack-loader', */
-                test: /\.(png|svg|jpe?g|gif)$/i,
+                test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
                     'file-loader',
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            bypassingOnDebug: true,
+                            bypassOnDebug: true,
                         },
-                    },
-                ],
-            },
-        ],
-    },
-};
+                    }
+                ]
+            }
+        ]
+    }
+}
