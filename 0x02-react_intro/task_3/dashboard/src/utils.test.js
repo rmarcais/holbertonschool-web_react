2a2ddadd934bf5508a774cf1f6ef0b3d22b688f0
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-09 15:41:03 +0200

    Update tests of utils functions and add tests for App and Notifications components

--- 0x02-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -4,8 +4,10 @@ describe('Basic tests of utils functions', () => {
     it('Tests the getFullYear function', () => {
         expect(getFullYear()).toBe(new Date().getFullYear());
     });
-    it('Tests the getFooterCopy function', () => {
+    it('Tests the getFooterCopy function with false', () => {
         expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+    });
+    it('Tests the getFooterCopy function with true', () => {
         expect(getFooterCopy(true)).toBe('Holberton School');
     });
     it('Tests the getLatestNotification function', () => {
