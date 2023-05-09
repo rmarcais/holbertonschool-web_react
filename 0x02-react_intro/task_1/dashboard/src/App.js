e35fc927e8056e67e85779ad417917f2b8c557c6
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-09 11:50:03 +0200

    Add all files from task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import logo from './holberton_logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
