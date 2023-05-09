3cad35f8db5a0415fbf99896dcf3e2c0e115c233
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-09 15:02:32 +0200

    Fix bug

--- 0x02-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x02-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
 import { configure } from 'enzyme';
-import Adaptater from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adaptater() });
+configure({ adapter: new Adapter() });
