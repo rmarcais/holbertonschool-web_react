d40194d076699165ef5970698a15fa7e6023aa63
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-21 20:51:45 +0200

    Add all files from task 4

---
+++ 0x04-React_component/task_4/dashboard/src/App/App.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from "./App";
+
+describe('Tests the App component', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+    it('Tests that App renders without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.exists()).toBe(true);
+    });
+    it('should contain the Notifications component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Notifications')).toHaveLength(1);
+    });
+    it('should contain the Header component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Header')).toHaveLength(1);
+    });
+    it('should contain the Login component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Login')).toHaveLength(1);
+    });
+    it('should contain the Footer component', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('Footer')).toHaveLength(1);
+    });
+    it('checks that CourseList is not displayed', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find('CourseList')).toHaveLength(0);
+    });
+    it('checks that the logOut function and the alert function is called with the good string', () => {
+        const mockLogOut = jest.fn();
+        const logger = jest.spyOn(window, 'alert');
+        expect(logger);
+        expect(mockLogOut);
+        jest.restoreAllMocks();
+    });
+});
+
+describe('Tests the App component when isLoggedIn is true', () => {
+    it('Tests that the Login component is not included.', () => {
+        const wrapper = shallow(<App isLoggedIn={true}/>);
+        expect(wrapper.find('Login')).toHaveLength(0);
+    });
+    it('should contain the Notifications component', () => {
+        const wrapper = shallow(<App isLoggedIn={true}/>);
+        expect(wrapper.find('CourseList')).toHaveLength(1);
+    });
+});
