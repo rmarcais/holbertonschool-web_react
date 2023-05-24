992eecd8680d81e4d29271411cfd8e177535112d
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-25 01:11:28 +0200

    Update package.json

--- 0x05-React_inline_styling/task_2/dashboard/src/index.js
+++ 0x05-React_inline_styling/task_2/dashboard/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootId = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>, rootId
 );
