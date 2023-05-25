b5beef0d01fac8b11e74b867d9299bf961563019
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-25 10:58:23 +0200

    Try with Élodie's tests

--- 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -1,41 +1,46 @@
-import React from "react";
+import React from 'react';
 import { shallow } from 'enzyme';
-import CourseList from "./CourseList";
+import CourseList from './CourseList';
 
-describe('Tests the CourseList component with an empty array', () => {
-    it('Tests that CourseList renders without crashing', () => {
-        const wrapper = shallow(<CourseList />);
-        expect(wrapper.exists()).toBe(true);
-    });
-    it('Tests that CourseList rendrers correctly if we do not pass a list prop', () => {
-        const wrapper = shallow(<CourseList />);
-        expect(wrapper.find('CourseListRow')).toHaveLength(3);
-    });
-});
+describe('CourseList component tests', () => {
 
-describe('Tests the CourseList component', () => {
-  let listCourses;
-  beforeEach(() => {
-    listCourses = [
-      {
-        id: 1,
-        name: 'ES6',
-        credit: 60
-      },
-      {
-        id: 2,
-        name: 'Webpack',
-        credit: 20
-      },
-      {
-        id: 3,
-        name: 'React',
-        credit: 40
-      }
-    ];
-  });
-  it('Tests that CourseList rendrers correctly if we pass a list prop', () => {
-    const wrapper = shallow(<CourseList listCourses={listCourses}/>);
-    expect(wrapper.find('CourseListRow')).toHaveLength(5);
-});
+    describe('When listCourse is not empty', () => {
+        let wrapper;
+
+        beforeEach(() => {
+            const listCourses = [
+                {id: 1, name: "ES6", credit: 60},
+                {id: 2, name: "Webpack", credit: 20},
+                {id: 3, name: "React", credit: 40},
+            ]
+            wrapper = shallow(<CourseList listCourses={listCourses}/>);
+        });
+
+        it('renders CourseList component without crashing', () => {
+            expect(wrapper.exists()).toBe(true);
+        });
+
+        it('renders 5 rows', () => {
+            const row = wrapper.find('CourseListRow');
+            expect(row).toHaveLength(5);
+        });
+    });
+
+    describe('When listCourse is empty', () => {
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper = shallow(<CourseList listCourses={[]}/>);
+        });
+
+        it('renders CourseList component without crashing', () => {
+            expect(wrapper.exists()).toBe(true);
+        });
+
+        it('renders only headers and text "No course available yet"', () => {
+            const row = wrapper.find('CourseListRow');
+            expect(row).toHaveLength(3);
+            expect(row.at(2).prop('textFirstCell')).toEqual('No course available yet');
+        });
+    });
 });
