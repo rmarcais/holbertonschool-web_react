ed128fd6bfa54e742242bc3b3d4d437cbbbcad2f
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-25 09:18:38 +0200

    Try with the package of Élodie

--- 0x05-React_inline_styling/task_0/dashboard/src/index.js
+++ 0x05-React_inline_styling/task_0/dashboard/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App/App';
 
-const rootId = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>, rootId
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
