0fabec4e137aa2e7ed08e94320a3a9c3241d0a47
Rémi Marçais <remi.marcais87@gmail.com>
2023-05-26 22:58:14 +0200

    Fix bugs

--- 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
-import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
 import NotificationItemShape from './NotificationItemShape';
 import NotificationItem from './NotificationItem';
