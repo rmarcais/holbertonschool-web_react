9acfb18bf377c31cb6e9955ca41494e2569f06f1
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-12 15:52:18 +0200

    Create and export map and map2

---
+++ 0x07-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+const map2 = map.set(2, 'Benjamin').set(4, 'Olivier');
+
+console.log(map2.toJS());
+
+export { map };
+export { map2 };
