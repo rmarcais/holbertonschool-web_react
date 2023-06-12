00f59cdee6a4d809a006bdfed7f2af10c43e74e1
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-12 13:43:58 +0200

    Remove exemple

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -4,10 +4,3 @@ export default function accessImmutableObject(object, array) {
     const nested = fromJS(object);
     return nested.getIn(array);
 }
-
-console.log(accessImmutableObject({
-    name: {
-         first: "Guillaume",
-         last: "Salva"
-    }
-}, ['name', 'first']));
