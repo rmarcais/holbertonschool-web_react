61f81025287fe5715c687f2b25c119b9968e5cd7
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-19 16:32:38 +0200

    Add all files from task 0 and write a basic reducer

---
+++ 0x09-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,48 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from "../actions/courseActionTypes";
+import { MARK_AS_READ, SET_TYPE_FILTER } from "../actions/notificationActionTypes";
+import {
+    LOGIN,
+    LOGOUT,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    DISPLAY_NOTIFICATION_DRAWER,
+    HIDE_NOTIFICATION_DRAWER
+} from "../actions/uiActionTypes";
+
+export const appInitialState = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+}
+
+export function uiReducer(state = appInitialState, action) {
+    switch (action.type) {
+        case 'DISPLAY_NOTIFICATION_DRAWER':
+            return {
+                ...state,
+                isNotificationDrawerVisible: true
+            };
+        case 'HIDE_NOTIFICATION_DRAWER':
+            return {
+                ...state,
+                isNotificationDrawerVisible: false
+            };
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                isUserLoggedIn: true
+            };
+        case 'LOGIN_FAILURE':
+            return {
+                ...state,
+                isUserLoggedIn: false
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                isUserLoggedIn: false
+            };
+        default:
+            return state;
+    }
+}
