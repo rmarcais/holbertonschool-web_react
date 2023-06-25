a9eb2fb9f1707f303eb8b04f539044a95bab8131
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-25 11:18:45 +0200

    Install the Redux DevTools extension

--- 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { uiReducer } from './reducers/uiReducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk'
 import App from './App/App';
 
-const store = createStore(uiReducer, applyMiddleware(thunk));
+const store = createStore(uiReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 const rootId = document.getElementById("root");
 ReactDOM.render(
