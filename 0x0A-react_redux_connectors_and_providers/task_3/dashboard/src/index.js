a21fc5f5841f7dc5b136a235737c3b994ffffc9f
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-25 11:31:30 +0200

    Modify the index.js to support the extension

--- 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { uiReducer } from './reducers/uiReducer';
-import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk'
 import App from './App/App';
 
-const store = createStore(uiReducer, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const rootId = document.getElementById("root");
 ReactDOM.render(
