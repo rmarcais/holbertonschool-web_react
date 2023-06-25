df0a65231d519955449a8bf43f33401d35893e26
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-25 15:03:32 +0200

    Connect notifications to the reducer

--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
@@ -4,10 +4,16 @@ import { StyleSheet, css } from 'aphrodite';
 import closeIcon from '../assets/close-icon.png';
 import NotificationItemShape from './NotificationItemShape';
 import NotificationItem from './NotificationItem';
+import { fetchNotifications } from '../actions/notificationActionCreators';
+import { connect } from 'react-redux';
 
 class Notifications extends React.PureComponent {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchNotifications();
   }
 
   render() {
@@ -61,6 +67,20 @@ Notifications.defaultProps = {
   handleHideDrawer: () => {},
   markNotificationAsRead: () => {}
 };
+
+export function mapStateToProps(state) {
+  return {
+    listNotifications: state.notifications
+  };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    fetchNotifications: () => dispatch(fetchNotifications())
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notifications)
 
 const opacityAnimationFrames = {
   '0%': {
@@ -131,4 +151,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Notifications;
+export { Notifications };
