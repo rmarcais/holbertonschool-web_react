4b26ba32aae2ecfcae8f64457eb593a86c64363c
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-25 14:38:20 +0200

    Add three action creators to notificationActionCreators.js

--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,4 +1,9 @@
-import { MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+import {
+    MARK_AS_READ,
+    SET_TYPE_FILTER,
+    SET_LOADING_STATE,
+    FETCH_NOTIFICATIONS_SUCCESS
+} from "./notificationActionTypes";
 
 export function markAsAread(index) {
     return {
@@ -11,5 +16,32 @@ export function setNotificationFilter(filter) {
     return {
         type: SET_TYPE_FILTER,
         filter
+    };
+}
+
+export function setLoadingState(loading) {
+    return {
+        type: SET_LOADING_STATE,
+        loading
+    };
+}
+
+export function setNotifications(data) {
+    return {
+        type: FETCH_NOTIFICATIONS_SUCCESS,
+        data
+    };
+}
+
+export function fetchNotifications() {
+    return function(dispatch) {
+        dispatch(setLoadingState(true));
+
+        return fetch('http://localhost:8564/notifications.json')
+        .then((data) => {
+            dispatch(setNotifications(data));
+        })
+        .catch((err) => console.log(err))
+        .finally(() => dispatch(setLoadingState(false)));
     };
 }
